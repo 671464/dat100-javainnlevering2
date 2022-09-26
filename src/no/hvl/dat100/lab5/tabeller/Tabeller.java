fae393ebf4c266c47b8ffd08f435eb69f3903085
hvl <hvl@158-37-192-211.hvl.no>
2022-09-26 22:51:45 +0200

    Ferdig b), e), g), h)

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -20,9 +20,17 @@ public class Tabeller {
 	public static String tilStreng(int[] tabell) {
 
 		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
+		String result = "[";		
+		for (int i = 0;i<tabell.length;i++) {
+			result = result + tabell[i];
+			if (i < tabell.length-1) {
+				result = result + ",";
+			}	
+		}			
 		
+		return result + ("]");	//Returnerer [1,4,6]
 	}
+	
 
 	// c) 2/3 Fullført
 	public static int summer(int[] tabell) {
@@ -68,12 +76,15 @@ public class Tabeller {
 		
 	}
 
-	// e)
+	// e) FERDIG
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] == tall) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	// f) - Fullført
@@ -88,28 +99,35 @@ public class Tabeller {
 		return tab;
 	}
 
-	// g) - FEIL
+	// g) - FERDIG
 	public static boolean erSortert(int[] tabell) {
-		boolean sortert = false;
-		int pos_1; int pos_2; 
-		int i = 0; 
-		int j = 1;
-		
-		//en for/while løkke som kjører tall for tall
-		//en if setning som endrer til true hvis sann for alle
-		do {
-			pos_1 = tabell[i];
-			pos_2 = tabell[j];
-			i++;
-			j++;
-			
-		} while (pos_1 < pos_2);
-			if (j==tabell.length) {
-				sortert = true;
+//		boolean sortert = false;
+//		int pos_1; int pos_2; 
+//		int i = 0; 
+//		int j = 1;
+//		
+//		//en for/while løkke som kjører tall for tall
+//		//en if setning som endrer til true hvis sann for alle
+//		do {
+//			pos_1 = tabell[i];
+//			pos_2 = tabell[j];
+//			i++;
+//			j++;
+//			
+//		} while (pos_1 < pos_2);
+//			if (j==tabell.length) {
+//				sortert = true;
+//			}
+//			
+//		
+//		return sortert;
+		for (int i = 0; i<tabell.length-1; i++)
+			if (tabell[i+1] < tabell[i]) {
+				return false;
 			}
-
+		return true;
 		
-		return sortert;
+		
 	}
 
 	// h)
@@ -119,22 +137,27 @@ public class Tabeller {
 		//throw new UnsupportedOperationException("settSammen ikke implementert");
 
 		int tab_lengde = tabell1.length + tabell2.length;
-		int [] tab = new int [tab_lengde];
-		//int t = 0;
+		int [] tab2 = new int [tab_lengde];
+		int teller = 0;
 		
-
-		for (int i = 0; i < tab.length; i++) {
-			while(i < tabell1.length) {
-				tab[i] = tabell1[i];
-				i++;
-			}
-
-				
-			
-			
+		// LEGGER TABELL 1 til i tab2
+		for (int i = 0; i < tabell1.length; i++) {
+			tab2[teller] = tabell1[i];
+			teller++;
+		}
+		// LEGGER TABELL 2 til i tab2
+		for (int i = 0; i < tabell2.length; i++) {
+			tab2[teller] = tabell2[i];
+			teller++;
 		}
 		
-		return tab;
+
+//		for (int i = 0; i < tab2.length; i++) {
+//			while(i < tabell1.length) {
+//				tab2[i] = tabell1[i];
+//				i++;
+		
+		return tab2;
 		
 	}
 }
